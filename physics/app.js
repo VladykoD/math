54c7090dc21873d3a991018505e3e6f7a663b1a2
VladykoD <VladykoD@gmail.com>
2021-01-20 02:30:01 +0300

    fireworks demo based on vector physics

--- physics/app.js
+++ physics/app.js
@@ -3,11 +3,6 @@
     const context = canvas.getContext(`2d`);
     let w, h;
     let centerY, centerX;
-    let position = vector.create(100, 100);
-    let velocity = vector.create(0,0)
-
-    velocity.setLength(3);
-    velocity.setAngle(Math.PI / 6)
 
     function init() {
         w = canvas.width = innerWidth * 2;
@@ -18,20 +13,50 @@
     }
     init();
 
-    window.addEventListener(`resize`, init);
+    window.addEventListener(`resize`, () => {
+        init();
+        rerun();
+    });
 
+    let particles = [];
+    const numParticles = 100;
+
+    function fillObject() {
+        for (let i = 0; i < numParticles; i++) {
+            particles.push(particle.create(centerX, centerY, Math.random() * 4 + 1, Math.random() * Math.PI * 2))
+        }
+    }
+    fillObject();
+
+    function rerun() {
+        particles.length = 0;
+        fillObject();
+    }
 
     function update() {
-        context.clearRect(0,0,w,h)
-
-        position.addTo(velocity)
-
-        context.beginPath()
-        context.arc(position.getX(), position.getY(), 10, 0, Math.PI * 2, false)
+        //context.clearRect(0,0,w,h)
+        context.fillStyle = 'rgba(2,37,52,.2)'
+        context.fillRect(0,0,w,h)
         context.fill();
+
+
+        for (let i = 0; i < numParticles; i++) {
+            let p = particles[i]
+            p.update()
+
+            context.beginPath()
+            context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false)
+            context.fillStyle = p.color;
+            context.fill();
+        }
 
         requestAnimationFrame(update)
     }
     update();
+
+
+    const btn = document.querySelector('.btn')
+    btn.addEventListener('click', rerun);
+
 
 })();
---
+++ physics/particle.js
@@ -0,0 +1,21 @@
+let particle = {
+    position: null,
+    velocity: null,
+
+    create: function (x, y, speed, direction) {
+        var obj = Object.create(this);
+
+        obj.position = vector.create(x, y)
+        obj.velocity = vector.create(0,0)
+        obj.velocity.setLength(speed)
+        obj.velocity.setAngle(direction)
+        obj.color = `hsl(${Math.random() * 50 | 0}, 80%, 60%)`
+
+        return obj
+    },
+
+    update: function() {
+        this.position.addTo(this.velocity)
+    }
+
+}
