99ebd88f7f8217acf79d3ab725d29198b7279034
VladykoD <VladykoD@gmail.com>
2021-01-20 01:41:07 +0300

    added script for vectors

---
+++ physics/app.js
@@ -0,0 +1,26 @@
+(() => {
+    const canvas = document.querySelector(`canvas`);
+    const context = canvas.getContext(`2d`);
+    let w, h;
+    let centerY, centerX;
+
+    function init() {
+        w = canvas.width = innerWidth * 2;
+        h = canvas.height = innerHeight * 2;
+
+        centerY = h / 2;
+        centerX = w / 2;
+    }
+    init();
+
+    window.addEventListener(`resize`, init);
+
+
+    function update() {
+        context.clearRect(0,0,w,h)
+
+        requestAnimationFrame(update)
+    }
+    update();
+
+})();
